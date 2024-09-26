6d85b45e29476f4f333f76c2beaeab94281629ad
unknown <budalabhagyaraj@gmail.com>
2024-09-26 11:12:09 +0530

    removed google client id

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,8 +6,7 @@ import Topics from "./screens/Topics";
 import Questions from "./screens/Questions";
 
 function App() {
-  const clientId =
-    "1086744707768-1mo9tdm3fad31g114u5sk0au2phuqkte.apps.googleusercontent.com";
+  const clientId = "your-cleint-id";
   return (
     <>
       <GoogleOAuthProvider clientId={clientId}>
